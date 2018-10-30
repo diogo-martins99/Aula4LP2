2ec4ee3d3f9c68d22e3e3c7f4e84ab1b8ea6aa30
Diogo Ferreira Martins <diogofm99@gmail.com>
2018-10-30 12:38:32 +0000

    Add Exercicio 3 - Solved

---
+++ Assets/TestScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    private Coroutine sayHello;
+    private Coroutine printNumbers;
+    private int n = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("PrintNumbers");
+        StartCoroutine("PrintHello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator PrintHello()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5);
+            print("Hello");
+        }
+    }
+
+    IEnumerator PrintNumbers()
+    {
+        while (true)
+        {
+            print(n);
+            n++;
+            yield return new WaitForSeconds(1);
+            if (n == 5) StopCoroutine("PrintHello");
+        }
+    }
+
+}
